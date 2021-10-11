7c48387568c9f619874a24fb988712aa9d28675e
Jagannath Bhat <jagannathbhatjb@gmail.com>
2021-10-11 22:29:50 +0530

    add courses section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css'
 
 import About from './components/About/index'
+import Courses from './components/Courses'
 
 function App() {
-  return <About />
+  return (
+    <>
+      <About />
+      <Courses />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Courses/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import SubscriptionForm from './SubscriptionForm'
+
+const index = () => {
+  return (
+    <div className='my-20 p-4 text-center w-full'>
+      <h1 className='alt-font font-bold mb-10 text-5xl'>courses</h1>
+      <div className='flex flex-col shadow-lg md:flex-row md:overflow-x-scroll md:h-80 md:w-full '>
+        <img
+          alt='git, gitlab, and github course for beginners or undergraduates'
+          src='/courses-git.png'
+        />
+        <div className='p-4 text-gray-600 md:text-left'>
+          <p className='mb-8 text-lg md:mb-2'>
+            i'm building the course and will publish it soon.
+          </p>
+          <div className='mb-8 md:mb-2'>
+            <p className='mb-4 text-lg'>
+              so gimme your email address and i'll notify you when it's
+              published.
+            </p>
+            <SubscriptionForm />
+          </div>
+          <div className='flex-1 p-2'>
+            <p className='mb-4 text-gray-600 text-lg'>
+              or join my discord and i'll send a message when it's published.
+            </p>
+            <a
+              href='https://discord.gg/PA5ZxAuSp3'
+              rel='noreferrer'
+              target='_blank'
+            >
+              <button className='button bg-green-500 flex mx-auto p-2 text-white md:mx-0'>
+                <i className='ri-discord-fill mr-2'></i> join discord
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default index
