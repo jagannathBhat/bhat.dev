30d17437851835c6eac68f844f1633fc63700a6b
Hugh Logan <hughlogan6969@gmail.com>
2021-10-24 16:52:01 +0530

    add footer section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/About/index'
 import Blog from './components/Blog'
 import Contact from './components/Contact'
 import Courses from './components/Courses'
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Courses />
       <Blog />
       <Contact />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { SOCIALS } from '../About/constants'
+
+const Footer = () => (
+  <div className='flex items-center justify-between p-8'>
+    <h1 className='alt-font font-bold mr-4 text-5xl text-green-500'>jb</h1>
+    <div className='text-3xl'>
+      {SOCIALS.map((social, index) => (
+        <a key={index} href={social.link} rel='noreferrer' target='_blank'>
+          <i className={`mx-3 ${social.icon}`}></i>
+        </a>
+      ))}
+    </div>
+  </div>
+)
+
+export default Footer
