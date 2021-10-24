bb4051a8723b10f67d34229fe479a35da8ce6312
Hugh Logan <hughlogan6969@gmail.com>
2021-10-24 14:02:33 +0530

    show date and link on the same line

--- src/components/Blog/Post.js
+++ src/components/Blog/Post.js
@@ -8,15 +8,17 @@ const Post = ({ data }) => {
         style={{ backgroundImage: `url('${data.image}')` }}
       ></div>
       <h3 className='alt-font font-semibold text-3xl'>{data.name}</h3>
-      <p className='mb-4 text-gray-400'>{data.date}</p>
-      <a
-        className='text-green-500 text-xl'
-        href={data.url}
-        rel='noreferrer'
-        target='_blank'
-      >
-        read post
-      </a>
+      <div className='flex items-center justify-between'>
+        <p className='text-gray-400'>{data.date}</p>
+        <a
+          className='text-green-500 text-xl'
+          href={data.url}
+          rel='noreferrer'
+          target='_blank'
+        >
+          read post
+        </a>
+      </div>
     </div>
   )
 }
