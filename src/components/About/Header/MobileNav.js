17e6e085924126111d1f5d28d704dc6240ba4f9f
Jagannath Bhat <jagannathbhatjb@gmail.com>
2024-09-08 11:38:29 +0530

    removed social links from menu

--- src/components/About/Header/MobileNav.js
+++ src/components/About/Header/MobileNav.js
@@ -1,36 +1,29 @@
-import React from 'react'
-import { LINKS } from '../constants'
-import { SOCIALS } from '../../constants'
+import React from "react";
+import { LINKS } from "../constants";
+import { SOCIALS } from "../../constants";
 
 const MobileNav = ({ setShowNav }) => {
   return (
-    <div className='bg-white fixed flex flex-col h-full items-center justify-center left-0 top-0 w-full'>
+    <div className="bg-white fixed flex flex-col h-full items-center justify-center left-0 top-0 w-full">
       <button
-        className='block fixed m-4 right-0 rounded-full px-3 py-2 text-2xl top-0
-          focus:bg-gray-500 focus:bg-opacity-50 md:hidden'
+        className="block fixed m-4 right-0 rounded-full px-3 py-2 text-2xl top-0
+          focus:bg-gray-500 focus:bg-opacity-50 md:hidden"
         onClick={() => setShowNav(false)}
       >
-        <i className='ri-close-line'></i>
+        <i className="ri-close-line"></i>
       </button>
       {LINKS.map((link, index) => (
         <a
           key={index}
-          className='m-3 text-xl'
+          className="m-3 text-xl"
           href={`#${link}`}
           onClick={() => setShowNav(false)}
         >
           {link}
         </a>
       ))}
-      <div className='mt-3 text-xl'>
-        {SOCIALS.map((social, index) => (
-          <a key={index} href={social.link} rel='noreferrer' target='_blank'>
-            <i className={`mx-3 ${social.icon}`}></i>
-          </a>
-        ))}
-      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MobileNav
+export default MobileNav;
