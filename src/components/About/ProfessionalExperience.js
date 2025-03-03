ec91876bd87d659d94218bc3ddb33b4dbdba5930
Jagannath Bhat <jagannathbhatjb@gmail.com>
2025-03-03 11:02:06 +0530

    updated professional experiences section (#50)

--- src/components/About/ProfessionalExperience.js
+++ src/components/About/ProfessionalExperience.js
@@ -3,17 +3,24 @@ import React from "react";
 import { PROFESSIONAL_EXPERIENCE } from "./constants";
 
 const ProfessionalExperience = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="flex flex-col space-x-0 space-y-12 md:flex-row md:space-x-7 md:space-y-0">
       {PROFESSIONAL_EXPERIENCE.map((exp, index) => (
         <div
           key={index}
-          className="font-semibold text-gray-500 px-4 md:w-1/2 md:px-0"
+          className="font-semibold text-gray-400 px-4 md:w-1/2 md:px-0"
         >
-          <img alt={exp.title} className="h-16 mb-2" src={exp.logo} />
           <p className="text-2xl text-black">{exp.title}</p>
+          <p>
+            <span className="text-gray-700">{exp.company}</span>
+            <span>{" . "}
+              {(exp.yearEnd ?? currentYear) - exp.yearStart} year
+              {(exp.yearEnd ?? currentYear) - exp.yearStart > 1 ? "s" : ""}
+            </span>
+          </p>
           <p>{exp.subtitle}</p>
-          <p>{exp.duration}</p>
         </div>
       ))}
     </div>
--- src/components/About/constants.js
+++ src/components/About/constants.js
@@ -13,15 +13,18 @@ export const LINKS = [
 
 export const PROFESSIONAL_EXPERIENCE = [
   {
+    company: "BigBinary",
     duration: "3 years",
-    logo: "https://www.bigbinary.com/blog_images/bigbinary_square_logo.png",
     subtitle: "Ruby on Rails, ReactJS, Kubernetes, Docker, AWS, Heroku",
     title: "Software Engineer",
+    yearStart: 2021,
   },
   {
+    company: "EY",
     duration: "1 year",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/EY_logo_2019.svg/1200px-EY_logo_2019.svg.png",
     subtitle: "Pen Testing, Burp Suite, Qualys",
     title: "Cybersecurity Analyst",
+    yearStart: 2020,
+    yearEnd: 2021,
   },
 ];
