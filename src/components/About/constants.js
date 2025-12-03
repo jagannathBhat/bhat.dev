9abbc838c085d5af7486c42ccaf4cfd16e1f39ee
Jagannath Bhat <jagannathbhatjb@gmail.com>
2025-12-03 23:32:58 +0530

    update professional experience

--- src/components/About/constants.js
+++ src/components/About/constants.js
@@ -2,29 +2,42 @@ export const LINKS = [
   { key: "about", title: "me me me me" },
   {
     key: "projects",
-    title: "you don't need to check them out. just hire me. you wont regret",
+    title: "you don't need to check them out. just hire me. you wont regret"
   },
   {
     key: "blog posts",
     title:
-      "if you have the time and attention span, i dare you to read them and respond",
-  },
+      "if you have the time and attention span, i dare you to read them and respond"
+  }
 ];
 
 export const PROFESSIONAL_EXPERIENCE = [
   {
-    company: "BigBinary",
-    duration: "3 years",
-    subtitle: "Ruby on Rails, ReactJS, Kubernetes, Docker, AWS, Heroku",
     title: "Software Engineer",
-    yearStart: 2021,
+    companies: [
+      {
+        name: "Rootly",
+        skills:
+          "Ruby on Rails, Turbo, Hotwire, Stimulus, PostgreSQL, Claude AI",
+        yearStart: 2024
+      },
+      {
+        name: "Neeto",
+        skills: "Ruby on Rails, ReactJS, Kubernetes, Docker, AWS, Heroku",
+        yearEnd: 2024,
+        yearStart: 2021
+      }
+    ]
   },
   {
-    company: "EY",
-    duration: "1 year",
-    subtitle: "Pen Testing, Burp Suite, Qualys",
     title: "Cybersecurity Analyst",
-    yearStart: 2020,
-    yearEnd: 2021,
-  },
+    companies: [
+      {
+        name: "EY",
+        skills: "Pen Testing, Burp Suite, Qualys",
+        yearEnd: 2021,
+        yearStart: 2020
+      }
+    ]
+  }
 ];
